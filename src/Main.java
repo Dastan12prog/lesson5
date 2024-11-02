b9e1ce05ea02f66deabd831f7618c72a12ce3efc
Lenovo <d75200794@gmail.com>
2024-11-02 22:26:51 +0600

    added new solution

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[] array = {3, 54, 22, -2, 0 , 3, 15, -10, 28};
+
+        int pos = 0;
+        int neg = 0;
+
+        for(int i= 0; i <= array.length; i++){
+
+            if(i % 2 == 0){
+                pos += 1;
+            }
+            else if(i < 0){
+                neg += 1;
+            }
         }
+        int[] list = {array.length};
+
+        System.out.println("Положительных чисел " + list[pos]);
+        System.out.println("Отрицательные чисел " + list[neg]);
     }
 }
